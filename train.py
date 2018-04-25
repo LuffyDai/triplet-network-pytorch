dfd5eecedc35203e48a275aabd8aff0f97391f33
daiqingfeng <fengnan2712@gmail.com>
2018-04-25 15:15:32 +0800

    fix

--- train.py
+++ train.py
@@ -17,6 +17,7 @@ from tensorboardX import SummaryWriter
 from logbook import Logger
 from nets import *
 import numpy as np
+from classifier_train import classifier
 
 logger = Logger('triplet-net')
 
@@ -76,22 +77,23 @@ def main():
                        ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
 
-    class Net(nn.Module):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-            self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-            self.conv2_drop = nn.Dropout2d()
-            self.fc1 = nn.Linear(320, 50)
-            self.fc2 = nn.Linear(50, 10)
+    mnist_train = torch.utils.data.DataLoader(
+        datasets.MNIST(
+            root='data', train=True, download=True,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
 
-        def forward(self, x):
-            x = F.relu(F.max_pool2d(self.conv1(x), 2))
-            x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-            x = x.view(-1, 320)
-            x = F.relu(self.fc1(x))
-            x = F.dropout(x, training=self.training)
-            return self.fc2(x)
+    mnist_test = torch.utils.data.DataLoader(
+        datasets.MNIST(
+            root='data', train=False, download=True,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
 
     model = MNISTNet()
     tnet = Tripletnet(model)
@@ -133,6 +135,15 @@ def main():
             'state_dict': tnet.state_dict(),
             'best_prec1': best_acc,
         }, is_best)
+
+    checkpoint_file = 'runs/%s/'%(args.name) + 'model_best.pth.tar'
+    assert os.path.isfile(checkpoint_file), 'Nothing to load...'
+    checkpoint_cl = torch.load(checkpoint_file)
+    model = MNISTNet()
+    tnet = Tripletnet(model)
+    tnet.load_state_dict(checkpoint_cl['state_dict'])
+    classifier(tnet.embeddingnet, mnist_train, mnist_test, writer)
+
     writer.close()
 
 def train(train_loader, tnet, criterion, optimizer, epoch):
