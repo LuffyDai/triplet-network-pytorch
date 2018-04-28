2aac32d72965b8cde4daec6be86ce4e88ed46bd8
daiqingfeng <fengnan2712@gmail.com>
2018-04-28 11:05:57 +0800

    Add logging_context

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,19 @@
 from misc import *
+import os
+from contextlib import contextmanager
+
+
+@contextmanager
+def logging_context(path=None, level=None):
+    from logbook import StderrHandler, FileHandler
+    from logbook.compat import redirected_logging
+    with StderrHandler(level=level or 'INFO').applicationbound():
+        if path:
+            if not os.path.isdir(os.path.dirname(path)):
+                os.makedirs(os.path.dirname(path))
+            with FileHandler(path, bubble=True).applicationbound():
+                with redirected_logging():
+                    yield
+        else:
+            with redirected_logging():
+                yield
