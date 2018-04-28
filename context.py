05209edbb707e126723a48de8e9955d9d3b31ea1
daiqingfeng <fengnan2712@gmail.com>
2018-04-28 10:52:31 +0800

    remove __init__

---
+++ context.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os
+
+os.environ['JOBLIB_TEMP_FOLDER'] = '/tmp'
+
+ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+from contextlib import contextmanager
+
+
+@contextmanager
+def Context(log=None, parallel=False, level=None):
+    from utils import logging_context
+    with logging_context(log, level=level):
+        if not parallel:
+            yield
+        else:
+            import joblib as jb
+            from multiprocessing import cpu_count
+            with jb.Paraller(n_jobs=cpu_count()) as par:
+                Context.parallel = par
+                yield
